dd76e626eaf88dcb9f5b2abebe7315c718a3957d
Brett Rudder <rudder.brett@gmail.com>
2017-01-06 11:48:07 -0600

    theme change

--- pelicanconf.py
+++ pelicanconf.py
@@ -5,12 +5,22 @@ from __future__ import unicode_literals
 AUTHOR = 'brett rudder'
 SITENAME = 'brudder-site'
 SITEURL = ''
+SITETITLE = "I'm interested in Machine Learning and AI"
 
 PATH = 'content'
 
 TIMEZONE = 'America/Chicago'
 
 DEFAULT_LANG = 'English'
+
+USE_FOLDER_AS_CATEGORY = False
+DEFAULT_CATEGORY = 'General'
+
+DISPLAY_PAGES_ON_MENU = False
+DISPLAY_CATEGORIES_ON_MENU = True
+
+# Paths
+STATIC_PATHS = ['images', 'authors']
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -19,17 +29,33 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+# # Blogroll
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('twitter', 'https://twitter.com/yourprofile'),
+         ('instagram', 'https://instagram.com/yourprofile'),
+         ('github', 'https://github.com/yourprofile'))
+
+SHARE_BUTTONS = ('twitter', 'facebook', 'whatsapp', 'mail')
 
 DEFAULT_PAGINATION = 10
+SUMMARY_MAX_LENGTH = 70
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+THEME = './themes/minimalX'
+
+TAG_CLOUD = True
+TAG_CLOUD_STEPS = 5
+TAG_CLOUD_MAX_ITEMS = 50
+TAG_CLOUD_BADGE = True
+TAG_CLOUD_SORTING = 'size'
+
+# Plugins
+PLUGIN_PATHS = ["plugins"]
+PLUGINS = ["tag_cloud", "autopages"]
